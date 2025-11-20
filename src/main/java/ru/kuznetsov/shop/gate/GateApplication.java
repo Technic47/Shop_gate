0434b38fd2313b89ad634b83e041e267c030559d
Pavel Kuznetsov <Technic47@gmail.com>
2025-11-20 20:13:48 +0300

    Dependency fixes.

--- src/main/java/ru/kuznetsov/shop/gate/GateApplication.java
+++ src/main/java/ru/kuznetsov/shop/gate/GateApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import ru.kuznetsov.shop.auth.config.KeycloakServiceConfiguration;
 import ru.kuznetsov.shop.business.access.config.DataAccessConfig;
+import ru.kuznetsov.shop.parameter.config.ParameterConfig;
 
 @SpringBootApplication
-@Import({DataAccessConfig.class, KeycloakServiceConfiguration.class})
+@Import({DataAccessConfig.class, KeycloakServiceConfiguration.class, ParameterConfig.class})
 public class GateApplication {
 
     public static void main(String[] args) {
