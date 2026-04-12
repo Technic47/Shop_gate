6ae6cc220b020c082e8f03f1184798f32e74004c
Pavel Kuznetsov <Technic47@gmail.com>
2026-04-12 18:57:19 +0300

    Order method update.

--- src/main/java/ru/kuznetsov/shop/gate/controller/business/order/OrderController.java
+++ src/main/java/ru/kuznetsov/shop/gate/controller/business/order/OrderController.java
@@ -1,14 +1,18 @@
 package ru.kuznetsov.shop.gate.controller.business.order;
 
 import jakarta.annotation.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.kuznetsov.shop.gate.config.PermissionsConfig;
 import ru.kuznetsov.shop.gate.controller.business.AbstractController;
 import ru.kuznetsov.shop.represent.contract.auth.AuthContract;
 import ru.kuznetsov.shop.represent.contract.order.OrderContract;
 import ru.kuznetsov.shop.represent.dto.order.OrderDto;
+import ru.kuznetsov.shop.represent.dto.order.OrderThinDto;
+import ru.kuznetsov.shop.represent.enums.OrderStatusType;
 
 import java.util.Collection;
 import java.util.Map;
@@ -19,6 +23,8 @@ import static ru.kuznetsov.shop.gate.enums.UserPermissionEnum.*;
 @RestController
 @RequestMapping("/order")
 public class OrderController extends AbstractController<OrderDto, OrderContract> {
+
+    Logger logger = LoggerFactory.getLogger(OrderController.class);
 
     public OrderController(OrderContract contractService, AuthContract authService, PermissionsConfig permissionsConfig) {
         super(contractService, authService, permissionsConfig);
@@ -51,5 +57,30 @@ public class OrderController extends AbstractController<OrderDto, OrderContract>
     @Override
     public ResponseEntity delete(String token, Long id) {
         return deleteInternal(token, id, ORDER_DELETE);
+    }
+
+    @GetMapping("/hasStatus")
+    public ResponseEntity<Collection<OrderThinDto>> getAllByStatusAndOptionalParams(
+            @RequestHeader(HttpHeaders.AUTHORIZATION) String token,
+            @RequestParam(value = "customerId", required = false) UUID customerId,
+            @RequestParam(value = "dateAfter", required = false) String dateAfter,
+            @RequestParam(value = "dateBefore", required = false) String dateBefore,
+            @RequestParam(value = "hasStatus") OrderStatusType hasStatus,
+            @RequestParam(value = "hasNotStatus") OrderStatusType hasNotStatus
+    ) {
+        logger.info("getAllByStatusAndOptionalParams start. CustomerId: {}, datAfter: {}, dateBefore: {}, hasStatus: {}, hasNotStatus: {}",
+                customerId, dateAfter, dateBefore, hasStatus, hasNotStatus);
+        if (hasAccess(token, ORDER_GET)) {
+            Collection<OrderThinDto> result = contractService.getAllByStatusAndOptionalParams(
+                    customerId,
+                    dateAfter,
+                    dateBefore,
+                    hasStatus,
+                    hasNotStatus
+            );
+            return result.isEmpty() ?
+                    ResponseEntity.noContent().build()
+                    : ResponseEntity.ok(result);
+        } else return ResponseEntity.status(401).build();
     }
 }
